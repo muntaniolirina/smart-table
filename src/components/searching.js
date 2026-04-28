1cf87248ed1b1b55f5c168fccb48d55f17cd973a
Irina Muntaniol <muntaniolirina@yandex.ru>
2026-04-28 20:36:17 +0300

    all done

--- src/components/searching.js
+++ src/components/searching.js
@@ -3,9 +3,16 @@ import {rules, createComparison} from "../lib/compare.js";
 
 export function initSearching(searchField) {
     // @todo: #5.1 — настроить компаратор
+    
+    const ruleNames = ['skipEmptyTargetValues'];
+
+    const compare = createComparison(ruleNames, [
+        rules.searchMultipleFields('search', ['date', 'customer', 'seller'], false)
+    ]);// Создаём компаратор на основе правил
 
     return (data, state, action) => {
         // @todo: #5.2 — применить компаратор
-        return data;
+        return data.filter(row => compare(row, state));
     }
 }
+
--- src/components/table.js
+++ src/components/table.js
@@ -12,12 +12,39 @@ export function initTable(settings, onAction) {
     const root = cloneTemplate(tableTemplate);
 
     // @todo: #1.2 —  вывести дополнительные шаблоны до и после таблицы
+    before.reverse().forEach((subName) => {
+        root[subName] = cloneTemplate(subName); // сохраняем клонированный объект в root
+        root.container.prepend(root[subName].container); // добавляем контейнер к таблице
+    });
+    after.forEach((subName) => {
+        root[subName] = cloneTemplate(subName);  // сохраняем клонированный объект в root
+        root.container.append(root[subName].container); // добавляем контейнер к таблице
+    })
 
     // @todo: #1.3 —  обработать события и вызвать onAction()
+    root.container.addEventListener('change', onAction);
+    root.container.addEventListener('reset', () => setTimeout(onAction, 1000));
+    root.container.addEventListener('submit', (event) => {
+        event.preventDefault();
+        onAction(event.submitter);
+    });
 
     const render = (data) => {
         // @todo: #1.1 — преобразовать данные в массив строк на основе шаблона rowTemplate
-        const nextRows = [];
+        const nextRows = data.map(item => {
+            const row = cloneTemplate(rowTemplate); //вернет объект с контейнером и элементами
+            Object.keys(item).forEach(key => { 
+                if (row.elements[key]) {
+                    if (row.elements[key].tagName.toLowerCase() === 'input' || 
+                        row.elements[key].tagName.toLowerCase() === 'select') {
+                        row.elements[key].value = item[key];
+                    } else {
+                        row.elements[key].textContent = item[key];
+                    }
+                }
+            });
+            return row.container;
+        });
         root.elements.rows.replaceChildren(...nextRows);
     }
 
